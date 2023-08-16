ec2e1fadec3a9e5914057483ed8a106b85788be7
leanhtai01 <leanhtai01@gmail.com>
2023-08-16 21:48:27 +0700

    initial commit

---
+++ archinstall/src/main/java/com/leanhtai01/archinstall/Main.java
@@ -0,0 +1,14 @@
+package com.leanhtai01.archinstall;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
+public class Main {
+    private static final Logger LOGGER = getLogger(lookup().lookupClass());
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello, leanhtai01!");
+    }
+}
