59bec0acdcdb676e072907c798ac2b188231a83d
leanhtai01 <leanhtai01@gmail.com>
2023-09-04 14:27:30 +0700

    add WirelessNetwork class

---
+++ archinstall/src/main/java/com/leanhtai01/archinstall/model/WirelessNetwork.java
@@ -0,0 +1,22 @@
+package com.leanhtai01.archinstall.model;
+
+import java.io.IOException;
+
+public class WirelessNetwork {
+    private String ssid;
+    private String password;
+    private String device;
+    private boolean isHidden;
+
+    public WirelessNetwork(String ssid, String password, String device, boolean isHidden) {
+        this.ssid = ssid;
+        this.password = password;
+        this.device = device;
+        this.isHidden = isHidden;
+    }
+
+    public void connect() throws InterruptedException, IOException {
+        new ProcessBuilder("iwctl", "--passphrase=%s".formatted(password), "station", device,
+                isHidden ? "connect-hidden" : "connect", ssid).inheritIO().start().waitFor();
+    }
+}
