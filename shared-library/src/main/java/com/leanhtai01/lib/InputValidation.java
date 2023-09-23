98dcd0b634a1fe55e43da68e3f4970f215cad64b
leanhtai01 <leanhtai01@gmail.com>
2023-09-23 15:39:00 +0700

    add class to validate user input

---
+++ shared-library/src/main/java/com/leanhtai01/lib/InputValidation.java
@@ -0,0 +1,35 @@
+package com.leanhtai01.lib;
+
+public final class InputValidation {
+    private InputValidation() {
+    }
+
+    public static int isValidInteger(String input, int from, int to) {
+        int number = Integer.parseInt(input);
+
+        if (number < from || number > to) {
+            throw new IllegalArgumentException("The input must be in range [%d, %d]".formatted(from, to));
+        }
+
+        return number;
+    }
+
+    public static String readPasswordFromConsole(String firstPrompt, String secondPrompt) {
+        System.console().printf(firstPrompt);
+        String password = String.valueOf(System.console().readPassword());
+
+        System.console().printf(secondPrompt);
+        String reenterPassword = String.valueOf(System.console().readPassword());
+
+        while (!password.equals(reenterPassword)) {
+            System.console().printf("Two password isn't the same. Please try again!");
+            System.console().printf(firstPrompt);
+            password = String.valueOf(System.console().readPassword());
+
+            System.console().printf(secondPrompt);
+            reenterPassword = String.valueOf(System.console().readPassword());
+        }
+
+        return password;
+    }
+}
