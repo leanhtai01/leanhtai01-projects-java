d7a9fd26b140d41ab71dec3ae56362cb6c2dfbe1
leanhtai01 <leanhtai01@gmail.com>
2023-09-23 19:46:07 +0700

    add class install remote desktop softwares

---
+++ archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/tool/RemoteDesktopInstall.java
@@ -0,0 +1,21 @@
+package com.leanhtai01.archinstall.osinstall.tool;
+
+import static com.leanhtai01.archinstall.util.PackageUtil.installPkgs;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.leanhtai01.archinstall.osinstall.SoftwareInstall;
+import com.leanhtai01.archinstall.systeminfo.UserAccount;
+
+public class RemoteDesktopInstall extends SoftwareInstall {
+    public RemoteDesktopInstall(String chrootDir, UserAccount userAccount) {
+        super(chrootDir, userAccount);
+    }
+
+    @Override
+    public int install() throws InterruptedException, IOException {
+        installPkgs(List.of("remmina", "freerdp", "spice-gtk", "libvncserver"), userAccount, chrootDir);
+        return 0;
+    }
+}
