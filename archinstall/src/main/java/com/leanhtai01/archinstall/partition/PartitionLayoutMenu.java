41b0737a0a20d18e1aaf561c475a00b8300b6322
leanhtai01 <leanhtai01@gmail.com>
2023-09-24 09:11:45 +0700

    move getPartitionLayout method to  menu option

--- archinstall/src/main/java/com/leanhtai01/archinstall/partition/PartitionLayoutMenu.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/partition/PartitionLayoutMenu.java
@@ -1,5 +1,8 @@
 package com.leanhtai01.archinstall.partition;
 
+import com.leanhtai01.archinstall.systeminfo.StorageDeviceSize;
+import com.leanhtai01.lib.InputValidation;
+
 public final class PartitionLayoutMenu {
     private PartitionLayoutMenu() {
     }
@@ -8,5 +11,28 @@ public final class PartitionLayoutMenu {
         System.console().printf("1. Unencrypted partition layout%n");
         System.console().printf("2. Unencrypted dual boot Windows partition layout%n");
         System.console().printf("? ");
+    }
+
+    public static PartitionLayout getPartitionLayout() {
+        System.console().printf("Enter disk's name (e.g. nvme0n1, sda): ");
+        String diskName = System.console().readLine();
+
+        System.console().printf("Enter swap size: ");
+        long swapSize = Long.parseLong(System.console().readLine());
+
+        int choice = InputValidation.chooseIntegerOption(PartitionLayoutMenu::displayPartitionLayoutSelectMenu, 1, 2);
+        PartitionLayout partitionLayout = null;
+
+        if (choice == 1) {
+            partitionLayout = new UnencryptedPartitionLayout(diskName,
+                    new StorageDeviceSize(550L, "M"), new StorageDeviceSize(550L, "M"),
+                    new StorageDeviceSize(swapSize, "G"));
+        } else if (choice == 2) {
+            partitionLayout = new UnencryptedDualBootWindowsPartitionLayout(
+                    diskName, new StorageDeviceSize(550L, "M"),
+                    new StorageDeviceSize(swapSize, "G"));
+        }
+
+        return partitionLayout;
     }
 }
