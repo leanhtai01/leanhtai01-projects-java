4e2d00bb09e10ba068a7bf125b42fab6ac7bc808
leanhtai01 <leanhtai01@gmail.com>
2023-09-29 17:21:58 +0700

    fix AUR package not available in main repos

--- archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/tool/BrowserInstall.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/tool/BrowserInstall.java
@@ -1,23 +1,26 @@
 package com.leanhtai01.archinstall.osinstall.tool;
 
-import static com.leanhtai01.archinstall.util.PackageUtil.installMainReposPkgs;
+import static com.leanhtai01.archinstall.util.PackageUtil.installPkgs;
 
 import java.io.IOException;
 import java.util.List;
 
 import com.leanhtai01.archinstall.osinstall.Installable;
+import com.leanhtai01.archinstall.systeminfo.UserAccount;
 
 public class BrowserInstall implements Installable {
     private String chrootDir;
+    private UserAccount userAccount;
 
-    public BrowserInstall(String chrootDir) {
+    public BrowserInstall(String chrootDir, UserAccount userAccount) {
         this.chrootDir = chrootDir;
+        this.userAccount = userAccount;
     }
 
     @Override
     public int install() throws InterruptedException, IOException {
-        installMainReposPkgs(List.of("torbrowser-launcher", "firefox-developer-edition", "firefox", "google-chrome"),
-                chrootDir);
+        installPkgs(List.of("torbrowser-launcher", "firefox-developer-edition", "firefox", "google-chrome"),
+                userAccount, chrootDir);
 
         return 0;
     }
