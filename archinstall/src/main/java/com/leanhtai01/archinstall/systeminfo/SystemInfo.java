c7e55a553fa415876816d90e3fab2b1f7ed58897
leanhtai01 <leanhtai01@gmail.com>
2023-09-29 14:23:38 +0700

    refine workspace for easy to add new things

--- archinstall/src/main/java/com/leanhtai01/archinstall/systeminfo/SystemInfo.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/systeminfo/SystemInfo.java
@@ -1,12 +1,8 @@
 package com.leanhtai01.archinstall.systeminfo;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 import com.leanhtai01.archinstall.partition.PartitionLayout;
-import com.leanhtai01.archinstall.partition.PartitionLayoutMenu;
-import com.leanhtai01.lib.InputValidation;
 
 public class SystemInfo {
     private String hostname;
@@ -54,22 +50,5 @@ public class SystemInfo {
 
     public void setPartitionLayout(PartitionLayout partitionLayout) {
         this.partitionLayout = partitionLayout;
-    }
-
-    public void getSystemInfo() throws IOException, InterruptedException {
-        System.console().printf("Hostname: ");
-        hostname = System.console().readLine();
-
-        rootPassword = InputValidation.readPasswordFromConsole(
-                "Root's password: ",
-                "Re-enter root's password: ",
-                "Two password isn't the same. Please try again!%n");
-
-        PartitionLayoutMenu partitionLayoutMenu = new PartitionLayoutMenu();
-        partitionLayout = partitionLayoutMenu.getPartitionLayout();
-
-        String[] mirrorsArray = new String[3];
-        Arrays.fill(mirrorsArray, "Server = https://mirror.xtom.com.hk/archlinux/$repo/os/$arch");
-        mirrors = Arrays.asList(mirrorsArray);
     }
 }
