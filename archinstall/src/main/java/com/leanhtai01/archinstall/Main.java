1b166dfdde43390845ac01c07eb8b8be18c204d1
leanhtai01 <leanhtai01@gmail.com>
2023-09-30 14:58:29 +0700

    add main

---
+++ archinstall/src/main/java/com/leanhtai01/archinstall/Main.java
@@ -0,0 +1,11 @@
+package com.leanhtai01.archinstall;
+
+import com.leanhtai01.archinstall.menu.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.selectOption();
+        mainMenu.doAction();
+    }
+}
