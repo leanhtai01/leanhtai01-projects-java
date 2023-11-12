6ac70b7ac896311bef8a2cec706b5c0daad89683
leanhtai01 <leanhtai01@gmail.com>
2023-11-12 20:43:08 +0700

    ask for confirmation before install flatpak pkgs

--- archinstall/src/main/java/com/leanhtai01/archinstall/menu/mainmenu/InstallFlatpakPackages.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/menu/mainmenu/InstallFlatpakPackages.java
@@ -1,14 +1,19 @@
 package com.leanhtai01.archinstall.menu.mainmenu;
 
+import static com.leanhtai01.archinstall.util.IOUtil.getConfirmation;
+import static com.leanhtai01.archinstall.util.IOUtil.isAnswerYes;
+
 import java.io.IOException;
 
 public class InstallFlatpakPackages implements Runnable {
     @Override
     public void run() {
-        try {
-            ConfigureSystem.installFlatpakPkgs();
-        } catch (InterruptedException | IOException e) {
-            Thread.currentThread().interrupt();
+        if (isAnswerYes(getConfirmation(":: Proceed with installation? [Y/n] "))) {
+            try {
+                ConfigureSystem.installFlatpakPkgs();
+            } catch (InterruptedException | IOException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
