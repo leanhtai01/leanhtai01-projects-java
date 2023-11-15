780d8ce841af4262dcb6dde17d3b64bcb23c9f44
leanhtai01 <leanhtai01@gmail.com>
2023-11-15 20:54:19 +0700

    add ktorrent to KDE Plasma

--- archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/desktopenvironment/KDEPlasma.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/desktopenvironment/KDEPlasma.java
@@ -27,7 +27,7 @@ public class KDEPlasma implements Installable {
                 "gwenview", "bluedevil", "khotkeys", "kinfocenter", "kscreen", "plasma-firewall", "plasma-nm",
                 "plasma-pa", "plasma-systemmonitor", "powerdevil", "sddm-kcm", "okular", "kcalc", "yakuake", "cryfs",
                 "plasma-vault", "discover", "breeze-gtk", "kde-gtk-config", "gnome-keyring", "krusader",
-                "kwalletmanager", "krename", "khelpcenter", "gtk2", "xdg-desktop-portal-kde"), chrootDir);
+                "kwalletmanager", "krename", "khelpcenter", "gtk2", "xdg-desktop-portal-kde", "ktorrent"), chrootDir);
         installFcitx5Bamboo();
 
         return 0;
