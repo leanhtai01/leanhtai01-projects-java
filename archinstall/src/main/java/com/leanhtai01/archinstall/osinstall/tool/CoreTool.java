4870939061625df3b386eae713b6252c2e4cfb38
leanhtai01 <leanhtai01@gmail.com>
2023-11-16 17:17:55 +0700

    remove game related packages

--- archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/tool/CoreTool.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/tool/CoreTool.java
@@ -1,7 +1,6 @@
 package com.leanhtai01.archinstall.osinstall.tool;
 
 import static com.leanhtai01.archinstall.util.ConfigUtil.enableService;
-import static com.leanhtai01.archinstall.util.PackageUtil.installMainReposPkgsWithOptionalDeps;
 import static com.leanhtai01.archinstall.util.PackageUtil.installPkgs;
 
 import java.io.IOException;
@@ -24,9 +23,7 @@ public class CoreTool implements Installable {
         installPkgs(List.of("keepassxc", "expect", "pacman-contrib", "dosfstools", "p7zip", "unarchiver",
                 "bash-completion", "flatpak", "tree", "archiso", "rclone", "rsync", "lm_sensors",
                 "ntfs-3g", "gparted", "exfatprogs", "pdftk", "youtube-dl", "ufw", "ufw-extras", "filezilla",
-                "texlive-most", "krusader", "gptfdisk", "ventoy-bin", "steam", "steam-native-runtime",
-                "wesnoth", "minecraft-launcher"), userAccount, chrootDir);
-        installMainReposPkgsWithOptionalDeps(List.of("lutris", "wine"), chrootDir);
+                "texlive-most", "krusader", "gptfdisk", "ventoy-bin"), userAccount, chrootDir);
 
         return 0;
     }
