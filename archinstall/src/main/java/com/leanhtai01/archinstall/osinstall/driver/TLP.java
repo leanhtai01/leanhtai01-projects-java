6a75d587bd22ec125ec798b21bc896d4757984f9
leanhtai01 <leanhtai01@gmail.com>
2023-12-25 14:45:22 +0700

    add TLP install class

---
+++ archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/driver/TLP.java
@@ -0,0 +1,35 @@
+package com.leanhtai01.archinstall.osinstall.driver;
+
+import static com.leanhtai01.archinstall.util.ConfigUtil.enableService;
+import static com.leanhtai01.archinstall.util.ConfigUtil.startService;
+import static com.leanhtai01.archinstall.util.PackageUtil.installMainReposPkgs;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.leanhtai01.archinstall.osinstall.Installable;
+
+public class TLP implements Installable {
+    private String chrootDir;
+
+    public TLP(String chrootDir) {
+        this.chrootDir = chrootDir;
+    }
+
+    @Override
+    public int install() throws InterruptedException, IOException {
+        installMainReposPkgs(List.of("tlp"), chrootDir);
+        return 0;
+    }
+
+    @Override
+    public int config() throws IOException, InterruptedException {
+        enableService("tlp", chrootDir);
+
+        if (chrootDir == null) {
+            startService("tlp", chrootDir);
+        }
+
+        return 0;
+    }
+}
