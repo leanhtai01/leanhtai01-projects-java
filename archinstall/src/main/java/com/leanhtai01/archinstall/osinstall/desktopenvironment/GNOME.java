73114fd6b8a14108da4fb484337f28c3831b77a9
leanhtai01 <leanhtai01@gmail.com>
2024-01-13 11:11:18 +0700

    add several packages

--- archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/desktopenvironment/GNOME.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/osinstall/desktopenvironment/GNOME.java
@@ -50,7 +50,8 @@ public class GNOME implements Installable {
                 "gnome-shell-extension-appindicator", "alacarte", "gedit", "gedit-plugins", "gnome-sound-recorder",
                 "seahorse", "seahorse-nautilus", "gnome-browser-connector", "xdg-desktop-portal",
                 "xdg-desktop-portal-gnome", "gnome-remote-desktop", "gnome-disk-utility", "libappindicator-gtk3",
-                "transmission-gtk", "power-profiles-daemon", "gvfs-smb", "evolution", "evolution-ews", "evolution-on"),
+                "transmission-gtk", "power-profiles-daemon", "gvfs-smb", "gvfs-google", "gvfs-mtp", "gvfs-nfs",
+                "gnome-user-share", "gnome-logs", "evolution", "evolution-ews", "evolution-on"),
                 chrootDir);
         installPkgs(List.of(IBUS_BAMBOO_PACKAGE), userAccount, chrootDir);
 
