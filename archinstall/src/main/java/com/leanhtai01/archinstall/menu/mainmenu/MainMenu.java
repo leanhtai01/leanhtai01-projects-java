afd3d968809fd6262e3925753a2d5c0db67dbe1d
leanhtai01 <leanhtai01@gmail.com>
2024-02-04 20:28:45 +0700

    delete install valve steam

--- archinstall/src/main/java/com/leanhtai01/archinstall/menu/mainmenu/MainMenu.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/menu/mainmenu/MainMenu.java
@@ -19,7 +19,6 @@ public class MainMenu extends SingleChoiceMenu {
         addOption(new Option("Install flatpak packages (installed system)", new InstallFlatpakPackages(), false));
         addOption(new Option("Install Lutris (installed system)", new InstallLutris(), false));
         addOption(new Option("Install VirtualBox (installed system)", new InstallVirtualBox(), false));
-        addOption(new Option("Install Valve Steam (installed system)", new InstallValveSteam(), false));
         addOption(new Option("Make Retail Windows ISO (installed system)", new MakeRetailWindowsISO(), false));
         addOption(new Option("Install Postfix (installed system, sudo)", new InstallPostfix(), false));
         addOption(new Option("Write Hybrid ISO to USB (sudo)", new WriteHybridISOToUSB(), false));
