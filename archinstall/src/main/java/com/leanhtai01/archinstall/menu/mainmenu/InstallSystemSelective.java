e29b9c8ecb36ef4a75176115d4724cddf3b88931
leanhtai01 <leanhtai01@gmail.com>
2024-02-16 10:48:34 +0700

    override method for install system selective

--- archinstall/src/main/java/com/leanhtai01/archinstall/menu/mainmenu/InstallSystemSelective.java
+++ archinstall/src/main/java/com/leanhtai01/archinstall/menu/mainmenu/InstallSystemSelective.java
@@ -1,5 +1,9 @@
 package com.leanhtai01.archinstall.menu.mainmenu;
 
+import javax.xml.xpath.XPathExpressionException;
+
+import com.leanhtai01.archinstall.util.ConfigReader;
+
 public class InstallSystemSelective extends InstallSystem {
     @Override
     protected void selectInstallSoftwares() {
@@ -8,6 +12,22 @@ public class InstallSystemSelective extends InstallSystem {
 
         System.console().printf("%n");
         driverMenu.selectOption();
+
+        System.console().printf("%n");
+        programmingMenu.selectOption();
+
+        System.console().printf("%n");
+        toolMenu.selectOption();
+
+        System.console().printf("%n");
+        virtualMachineMenu.selectOption();
+    }
+
+    @Override
+    protected void selectInstallSoftwareFromFile(ConfigReader configReader)
+            throws NumberFormatException, XPathExpressionException {
+        desktopEnvironmentMenu.setOptions(configReader.getDesktopEnvironmentOptions());
+        driverMenu.setOptions(configReader.getDriverOptions());
 
         System.console().printf("%n");
         programmingMenu.selectOption();
